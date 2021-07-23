b3db7fc880b1a49e8d0ae0ff350c5a9a29e3eb4f
zyzyxzy <32243723+zyzyxzy@users.noreply.github.com>
2021-07-23 16:13:32 -0400

    removed timeline back

--- tasks/symbolcount/task-forqual.js
+++ tasks/symbolcount/task-forqual.js
@@ -317,5 +317,14 @@ practice_trial.timeline = [fixation, symbols_sequence, response, practice_feedba
 practice_trial.repetitions = practice_trials
 
 // create timeline and events/objects for study (the first next lines are always the same! consent then check whether it's same person)
-
+var timeline = [];
+    //timeline = create_consent(timeline, taskinfo);
+    //timeline = check_same_different_person(timeline);
+    
+    timeline.push(instructions);
+    timeline.push(practice_trial);
+    timeline.push(instructions2);
+    timeline.push(trial);
+    timeline.push(debrief_block);
+    timeline = create_demographics(timeline);
 // run task - embedded
