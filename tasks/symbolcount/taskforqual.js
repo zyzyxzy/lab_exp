bafb2422db31089d0fee7d646ad255cd96f9613e
zyzyxzy <32243723+zyzyxzy@users.noreply.github.com>
2021-07-23 16:37:54 -0400

    replaced with own url

--- tasks/symbolcount/taskforqual.js
+++ tasks/symbolcount/taskforqual.js
@@ -1,3 +1,9 @@
+/**
+ * task.js
+ * Hause Lin, Maham Khan, Frank Fan
+ * 2020
+ **/
+
 var info_ = {
   subject: jsPsych.randomization.randomID(15), // random subject id
   type: "task", // 'task', 'survey', or 'study'
@@ -172,7 +178,7 @@ var fixation = {
   // define fixation
   type: "image-keyboard-response",
   // stimulus: "<div style='font-size:30px;'>&#9679</div>", // dot as fixation
-  stimulus: "https://zyzyxzy.github.io/lab_exp/tasks/symbolcount/fixation_white.png", // dot as fixation
+  stimulus: "https://dazzling-engelbart-fa84ed.netlify.app/fixation_white.png", // dot as fixation
   stimulus_height: 30,
   stimulus_width: 30,
   choices: jsPsych.NO_KEYS,
@@ -367,3 +373,7 @@ var debrief_block = {
   },
 };
 timeline.push(debrief_block);
+
+// Make timeline accessible from Qualtrics JavaScript
+window.symbolCountTimeline = timeline;
+{"mode":"full","isActive":false}
