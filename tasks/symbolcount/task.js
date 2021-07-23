652cd662508dfd64e8d117875c41c82a476eb30d
zyzyxzy <32243723+zyzyxzy@users.noreply.github.com>
2021-07-23 12:00:04 -0400

    removed consent

--- tasks/symbolcount/task.js
+++ tasks/symbolcount/task.js
@@ -318,8 +318,8 @@ practice_trial.repetitions = practice_trials
 
 // create timeline and events/objects for study (the first next lines are always the same! consent then check whether it's same person)
 var timeline = [];
-timeline = create_consent(timeline, taskinfo);
-timeline = check_same_different_person(timeline);
+//timeline = create_consent(timeline, taskinfo);
+//timeline = check_same_different_person(timeline);
 
 timeline.push(instructions);
 timeline.push(practice_trial);
